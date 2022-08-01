9b9edbe34240dfe6484bd71c790fb254a4bfcce4
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2022-08-01 13:23:25 +0930

    Update assemble.sh

--- scripts/assemble.sh
+++ scripts/assemble.sh
@@ -11,5 +11,5 @@
 module load python3-as-python
 module load megahit/1.2.9
 
-megahit -1 Reads/reads_1.fq.gz -2 Reads/reads2.fq.gz --k-min 21 --k-max 141 -o MEGAHIT_output -t 56
+megahit -1 Reads/reads_1.fq.gz -2 Reads/reads_2.fq.gz --k-min 21 --k-max 141 -o MEGAHIT_output -t 56
 
